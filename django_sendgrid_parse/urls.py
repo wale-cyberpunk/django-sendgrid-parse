e9468c763d72d630e65d3233e930e577fbd71247
Andrey-np <spotmax2019@gmail.com>
2022-01-25 18:50:03 +0200

    .

--- django_sendgrid_parse/urls.py
+++ django_sendgrid_parse/urls.py
@@ -1,11 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'django_sendgrid_parse'
 urlpatterns = [
-    url(
-        r'^$',
-        views.sendgrid_email_receiver, name='webhook'
-    ),
+    path('', views.sendgrid_email_receiver, name='webhook'),
 ]
